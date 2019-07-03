0aa9b6179b9b488a477782574c6f1a0c1b7009a8
Tony Kwok <holypaladin07@gmail.com>
2019-07-03 01:48:15 -0700

    trying to resolve vulnabilities

--- App.js
+++ App.js
@@ -4,12 +4,20 @@ import { TabNavigator } from 'react-navigation';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import AuthScreen from './screens/AuthScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
 
 export default function App() {
 
   const MainNavigator = TabNavigator({
     welcome: { screen: WelcomeScreen },
-    auth: { screen: AuthScreen }
+    auth: { screen: AuthScreen },
+    main: { 
+      screen: TabNavigator({
+        map: { screen: MapScreen },
+        deck: { screen: DeckScreen }
+      })
+    }
   });
 
   return (
