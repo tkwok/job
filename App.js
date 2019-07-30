69e07282af05468b2281d07058ead21882ff82e9
Tony Kwok <holypaladin07@gmail.com>
2019-07-29 21:51:39 -0700

    finally functioning on expo 2 on phone 2

--- App.js
+++ App.js
@@ -1,45 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator, StackNavigator } from 'react-navigation';
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ReviewScreen from "./screens/ReviewScreen";
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
-import WelcomeScreen from './screens/WelcomeScreen';
-import AuthScreen from './screens/AuthScreen';
-import MapScreen from './screens/MapScreen';
-import DeckScreen from './screens/DeckScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import ReviewScreen from './screens/ReviewScreen';
-
-export default function App() {
-
-  const MainNavigator = TabNavigator({
-    welcome: { screen: WelcomeScreen },
-    auth: { screen: AuthScreen },
-    main: { 
-      screen: TabNavigator({
-        map: { screen: MapScreen },
-        deck: { screen: DeckScreen },
+const MainNavigator = createAppContainer(createBottomTabNavigator({  // set createAppContainer here
+    welcome: WelcomeScreen,
+    auth: AuthScreen,
+    main: {
+    screen: createBottomTabNavigator({
+        map: MapScreen,
+        deck: DeckScreen,
         review: {
-          screen: StackNavigator({
-            review: { screen: ReviewScreen },
-            settings: { screen: SettingsScreen }
-          })
+        screen: createStackNavigator({
+            review: ReviewScreen,
+            settings: SettingsScreen
+        })
         }
-      })
+    })
     }
-  });
+}));
 
-  return (
-    <View style={styles.container}>
-      <MainNavigator />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  }
 });
+
+
