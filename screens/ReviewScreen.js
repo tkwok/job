fa981df056280e9b27c5cb5b04bdaf2825217003
Tony Kwok <holypaladin07@gmail.com>
2019-07-29 22:28:47 -0700

    fixed navigation syntax

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -3,24 +3,40 @@ import { View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
-    static navigationOptions = {
-        title: 'Review Jobs',
-        header: ({ navigate }) => {
-            return {
-                right: (
-                    <Button 
-                        title="Settings" 
-                        onPress={() => navigate('settings')}
-                        backgroundColor="rgba(0,0,0,0)"
-                        color="rgba(0, 122,255, 1)"
-                    />
-                ),
-                style: {
-                    marginTop: Platform.OS === 'android'? 24: 0
-                }
-            };
-        }
-    };
+    static navigationOptions = ({ navigation }) => {
+        return {
+          headerRight: (
+            <Button
+                title="Settings"
+                onPress={() => navigation.navigate('settings')}
+                backgroundColor="rgba(0,0,0,0)"
+                color="rgba(0, 122,255, 1)"
+            />
+          ),
+          style: {
+                marginTop: Platform.OS === 'android'? 24: 0
+          }
+        };
+      };
+
+    // static navigationOptions = {
+    //     title: 'Review Jobs',
+    //     header: ({ navigate }) => {
+    //         return (
+    //             right: (
+    //                 <Button 
+    //                     title="Settings" 
+    //                     onPress={() => navigate('settings')}
+    //                     backgroundColor="rgba(0,0,0,0)"
+    //                     color="rgba(0, 122,255, 1)"
+    //                 />
+    //             ),
+    //             style: {
+    //                 marginTop: Platform.OS === 'android'? 24: 0
+    //             }
+    //         );
+    //     }
+    // };
 
     render() {
         return (
