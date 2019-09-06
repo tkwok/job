66c7410d7876fe0f95e952e3c02265c3d258635d
Tony Kwok <holypaladin07@gmail.com>
2019-09-05 22:05:24 -0700

    added deck screen

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
 
 class DeckScreen extends Component {
     render() {
         return (
             <View>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
             </View>
         );
     }
 }
 
-export default DeckScreen;
+function mapStateToProps({ jobs }) {
+    return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps)(DeckScreen);
