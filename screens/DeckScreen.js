b5bf0635f159a6ca60b76917539ef7c03161503a
Tony Kwok <holypaladin07@gmail.com>
2019-09-07 22:28:37 -0700

    continue template work on card view for deck screen

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
+import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
+    renderCard(job) {
+        return (
+            <Card title={job.jobtitle}>
+                <View style={StyleSheet.detailWrapper}>
+                    <Text>{job.company}</Text>
+                    <Text>{job.formattedRelativeTime}</Text>
+                </View>
+                <Text>
+                    {job.snippet.replace()}
+                </Text>
+            </Card>
+        )
+    }
+
     render() {
         return (
             <View>
+                <Swipe
+                    data={this.props.jobs}
+                    renderCard={this.renderCard}
+                />
             </View>
         );
+    }
+}
+
+const styles = {
+    detailWrapper: {
+        flexDirection: 'row',
+        justifiedContent: 'space-around'
     }
 }
 
