56e32e3b3cfaacbcb43be6078f103096930967cc
Tony Kwok <holypaladin07@gmail.com>
2019-09-09 21:47:10 -0700

    added MapView

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 import Swipe from '../components/Swipe';
+import { MapView } from 'expo';
+import { Platform } from '@unimodules/core';
 
 class DeckScreen extends Component {
     renderCard(job) {
+        const initialRegion = {
+            longitude: job.longitude,
+            latitude: job.latitude,
+            latitudeDelta: 0.045,
+            longitudeDelta: 0.02
+        };
+
         return (
             <Card title={job.jobtitle}>
+                <View style={{ height: 300 }}>
+                    <MapView 
+                        scrollEnabled={false}
+                        style={{ flex: 1}}>
+                        cacheEnabled={Platform.OS === 'android' ? true : false}
+                        initialRegion={initialRegion}
+                    </MapView>
+                </View>
                 <View style={StyleSheet.detailWrapper}>
                     <Text>{job.company}</Text>
                     <Text>{job.formattedRelativeTime}</Text>
                 </View>
                 <Text>
-                    {job.snippet.replace()}
+                    {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
                 </Text>
             </Card>
         )
+    }
+
+    renderNoMoreCards() {
+        return (
+            <Card title="No More Jobs">
+            </Card>
+        );
     }
 
     render() {
@@ -24,6 +48,7 @@ class DeckScreen extends Component {
                 <Swipe
                     data={this.props.jobs}
                     renderCard={this.renderCard}
+                    renderNoMoreCards={this.renderNoMoreCards}
                 />
             </View>
         );
