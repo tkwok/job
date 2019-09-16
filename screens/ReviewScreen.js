8cf72e6b2591460db342c809f86d43ecf645583c
Tony Kwok <holypaladin07@gmail.com>
2019-09-15 17:57:30 -0700

    destructure job object in reviewscreen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Platform, ScrollView } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, Text, Platform, ScrollView, Linking } from 'react-native';
+import { Button, Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 class ReviewScreen extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -14,20 +14,27 @@ class ReviewScreen extends Component {
             />
           ),
           style: {
-                marginTop: Platform.OS === 'android'? 24: 0
+            marginTop: Platform.OS === 'android'? 24: 0
           }
         };
       };
 
     renderLikedJobs() {
       return this.props.likedJobs.map(job => {
+        const { company, formattedRelativeTime, url } = job;
+        
         return (
           <Card>
             <View styles={{ height: 200 }}>
               <View style={styles.detailWrapper}>
-                <Text style={styles.italics}>{job.company}</Text>
-                <Text style={styles.italics}>{job.formattedRelativeTime}</Text>
+                <Text style={styles.italics}>{company}</Text>
+                <Text style={styles.italics}>{formattedRelativeTime}</Text>
               </View>
+              <Button
+                title="Apply Now"
+                backgroundColor="#03A9F4"
+                onPress={() => Linking.openURL(url)}
+              />
             </View>
           </Card>
         );
